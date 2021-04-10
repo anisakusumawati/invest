700855ac2dc522ea61a65da7303f7ac9632c01b1
anisakusumawati <80797485+anisakusumawati@users.noreply.github.com>
2021-04-10 18:57:51 +0700

    Add files via upload

---
+++ home.java
@@ -0,0 +1,11 @@
+package inventaris;
+
+public class home {
+        public static void main(String[] args) {
+            product product = new product();
+            product.menu();
+            product.getnilaidata();
+        }
+    }
+
+
